a442682a9577ed2a371fe48b73aacf563c5dc84a
parasmani300 <parasmani300@gmail.com>
2024-06-17 20:55:37 +0530

    Custom Authentication provider in action

--- rest-services/src/main/java/com/secure/rest_services/service/CustomerService.java
+++ rest-services/src/main/java/com/secure/rest_services/service/CustomerService.java
@@ -3,15 +3,26 @@ package com.secure.rest_services.service;
 import com.secure.rest_services.dao.CustomerDao;
 import com.secure.rest_services.model.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+/**
+* This service is supposed to be used when DaoAuthentication provider is supposed
+ * to be used. When CustomAuthenticationProvider is used. It's of no use. It can
+ * be deleted if required.
+*  */
 @Service
 public class CustomerService {
     @Autowired
     CustomerDao customerDao;
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     public Customer registerUser(Customer customer) throws Exception {
         try {
+            String hashedPassword = passwordEncoder.encode(customer.getPwd());
+            customer.setPwd(hashedPassword);
             Customer savedCustomer = customerDao.save(customer);
             return savedCustomer;
         }catch (Exception ex){
